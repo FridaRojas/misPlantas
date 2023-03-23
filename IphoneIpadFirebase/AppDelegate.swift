e0626e197c91290dcae09197251f586f2ade1259
Frida Rojas <frida.rojasa@alumno.buap.mx>
2023-03-22 19:20:14 -0600

    Añade Notificaciones Push, falta ponerlas en tiempo

--- IphoneIpadFirebase/AppDelegate.swift
+++ IphoneIpadFirebase/AppDelegate.swift
@@ -5,29 +5,55 @@
 //  Created by Frida Rojas Alarcon on 16/01/23.
 //
 
-import Foundation
-import UIKit
+//import Foundation
+//import UIKit
 import SwiftUI
 import FirebaseCore
 import GoogleSignIn
 import FacebookLogin
+import FirebaseMessaging
 
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    //firebase
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         FirebaseApp.configure()
+        requestAuthorizationForPushNotification(application: application)
         //GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
         return true
     }
     
+    //login en google
     @available(iOS 9.0, *)
     func application(_ application: UIApplication, open url: URL,
-                     options: [UIApplication.OpenURLOptionsKey: Any])
-    -> Bool {
+                     options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
         return GIDSignIn.sharedInstance.handle(url)
     }
+    
+    //'activar' las notificaciones
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    //permisos de notificaciones
+    private func requestAuthorizationForPushNotification(application: UIApplication){
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]){ _, _ in}
+        application.registerForRemoteNotifications()
+    }
+    
+    //para
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    
 }
 
 
